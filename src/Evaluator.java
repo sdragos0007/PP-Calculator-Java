ffe183b1a9443996eeee884b390a6dfe173fd6b8
Dragos <waggypetshop@gmail.com>
2026-02-25 15:12:39 +0200

    chore: setup schelet arhitectural pentru Shunting Yard si Evaluator

---
+++ src/Evaluator.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Evaluator {
+
+    // Calculează rezultatul pe baza listei
+    public double evaluate(List<String> rpnTokens) {
+        // Folosim ArrayDeque ca o stivă explicită (mai eficient decât java.util.Stack)
+        Deque<Double> stack = new ArrayDeque<>();
+
+        // TODO: Implementarea evaluării matematice
+        return 0.0;
+    }
+}
---
+++ src/ShuntingYardParser.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ShuntingYardParser {
+
+    // Transformă o expresie Infix într-o listă
+    public List<String> parse(String expression) {
+        // TODO: Implementarea algoritmului Shunting Yard
+        return null;
+    }
+}
