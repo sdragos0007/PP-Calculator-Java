441438efde096f221d61b6debb2bb5a3244e24bc
Dragos <waggypetshop@gmail.com>
2026-02-25 15:55:13 +0200

    feat: implementare evaluator matematic cu stiva explicita

--- src/Evaluator.java
+++ src/Evaluator.java
@@ -1,15 +1,44 @@
-import java.util.List;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
+import java.util.Arrays;
 
 public class Evaluator {
 
-    // Calculează rezultatul pe baza listei
-    public double evaluate(List<String> rpnTokens) {
-        // Folosim ArrayDeque ca o stivă explicită (mai eficient decât java.util.Stack)
+    public double evaluate(List<String> Tokens) {
+        // Folosim ArrayDeque ca o stivă explicită
         Deque<Double> stack = new ArrayDeque<>();
 
-        // TODO: Implementarea evaluării matematice
-        return 0.0;
+        for (String token : Tokens) {
+
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+
+                double right = stack.pop();
+                double left = stack.pop();
+
+                switch (token) {
+                    case "+": stack.push(left + right); break;
+                    case "-": stack.push(left - right); break;
+                    case "*": stack.push(left * right); break;
+                    case "/": stack.push(left / right); break;
+                }
+            } else {
+                stack.push(Double.parseDouble(token));
+            }
+        }
+        return stack.pop();
     }
+
+    // Metodă temporară pentru testare
+//    public static void main(String[] args) {
+//        Evaluator evaluator = new Evaluator();
+//
+//        // Aceasta este lista exactă pe care am obținut-o din ShuntingYardParser
+//        // pentru expresia "2*(4-5/6)/456"
+//        List<String> testTokens = Arrays.asList("2", "4", "5", "6", "/", "-", "*", "456", "/");
+//
+//        double result = evaluator.evaluate(testTokens);
+//        System.out.println("Lista RPN: " + testTokens);
+//        System.out.println("Rezultat calculat: " + result);
+//    }
 }
